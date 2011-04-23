cf48892d14b79461c3c5de096688679a49385bee
Andreas Meingast <ameingast@gmail.com>
2011-04-23 22:13:22 +0200

    Introduced concatWith

--- Yawn/Util.hs
+++ Yawn/Util.hs
@@ -1,8 +1,11 @@
 module Yawn.Util (
-  split
+  split,
+  concatWith
 ) where
 
 split :: (a -> Bool) -> [a] -> [[a]]
 split _ [] = []
 split f xs  = let (y, ys) = break f xs in y : split f (dropWhile f ys) 
 
+concatWith :: Show a => String -> [a] -> String
+concatWith c xs = foldl (\a b -> a ++ c ++ (show b)) "" xs
